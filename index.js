e0162d4af4db6eb0ef2cf9a868f85b867bdadf34
Vladimir Uram <vladimir.uram@globallogic.com>
2018-04-12 14:07:49 +0200

    added express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require('http');
-var port = 8010;
-var server = http.createServer(
-    function(request, response){
+const http = require('http');
+const port = 8010;
+const server = http.createServer();
+  server.on('request', (request, response) => {
         //ak pride nejaky request tak sa otvori tato fukcia
 //urcim aky bude response a nastavim mu content napr obrazok html dokument cookies atd...
 //adress() nabindova andresa
@@ -12,6 +12,7 @@ response.writeHead(200);//vratim 200 code
     
    
 });
+server.on('error', err=>console.log(err));
 
 server.listen(port);// lsitener na porte 8010
 console.log(`Running at http://localhost:${server.address().port}`)
