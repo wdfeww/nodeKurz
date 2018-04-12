e473d06368b0166c9d51c62dd153d55e585e733f
Vladimir Uram <vladimir.uram@globallogic.com>
2018-04-12 16:16:38 +0200

    handlebars with hardcoded data

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
 const http = require('http');
+const express = require('express');
 const port = 8010;
-const server = http.createServer();
-  server.on('request', (request, response) => {
-        //ak pride nejaky request tak sa otvori tato fukcia
-//urcim aky bude response a nastavim mu content napr obrazok html dokument cookies atd...
-//adress() nabindova andresa
-console.log(request);//vypisem request
-response.writeHead(200);//vratim 200 code
-    response.write('Hello world ');
-    response.end();
-    
-   
-});
-server.on('error', err=>console.log(err));
 
-server.listen(port);// lsitener na porte 8010
-console.log(`Running at http://localhost:${server.address().port}`)
+const app = express();
+
+const catalog =[
+    
+{
+    id:'telefony',
+    name:'Phones',
+    items:[
+        {id:'iphone', name:'Iphone X', price:999},
+        {id:'s9', name:'Samsung S9', price:199}
+    ]
+},
+
+{  
+    id:'tablety',
+    name:'Tablets',
+    items:[
+        {id:'ipad', name:'Ipad Pro', price:993},
+        {id:'s10', name:'Samsung S10', price:599}
+    ]
+}
+];
+
+const expressHandlebars = require('express-handlebars');
+const handlebars = expressHandlebars.create({
+    layoutsDir: 'views/layouts',
+    partialsDir: 'views/partials',
+    defaultLayout: 'main'
+});
+
+
+app.engine('handlebars', handlebars.engine);//instalujem engine handlebars
+app.set('views', 'views');
+app.set('view engine', 'handlebars');
+
+app.use(( req, res, next)=>{
+    console.log('path', req.url);
+    next();
+});
+
+// app.use(express.static('public'));//ked dam localhost/nejaky html tak mi ho otvori
+
+app.get('/',(req,res)=>{
+    res.render('home/index');
+    // console.log("Hello World from INDEX");
+    // res.type('txt');
+    // res.status(200);
+    // res.send('Hello Index');
+
+});
+app.get('/kategorie',(req,res)=>{
+    res.render('catalog/categories', {catalog})
+});
+
+app.get('/kategoria/:categoryId',(req,res)=>{
+    const category = catalog.find(c => c.id === req.params.categoryId);
+    res.render('catalog/category', {category})
+});
+
+app.get('/product/:categoryId/:productId',(req,res)=>{
+    const category = catalog.find(c => c.id === req.params.categoryId);
+    const product = category.items.find(c => c.id === req.params.productId);
+    res.render('catalog/product',{category, product})
+});
+
+app.listen(port);
+console.log(`Running at http://localhost:${port}`)
